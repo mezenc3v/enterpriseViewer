5dd11d16d3890bfce9912162a00068fc641bf472
DESKTOP-4GBES0I\r0m-1 <mezenc3v@gmail.com>
2018-08-26 20:24:37 +0300

    Added client validation

--- EnterpriseViewer/EnterpriseViewer.WinForms/Presenters/DepartmentView.cs
+++ EnterpriseViewer/EnterpriseViewer.WinForms/Presenters/DepartmentView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using EnterpriseViewer.Model;
 
 namespace EnterpriseViewer.WinForms.Presenters
@@ -13,30 +14,37 @@ namespace EnterpriseViewer.WinForms.Presenters
 			Employees = employees ?? throw new ArgumentNullException(nameof(employees));
 		}
 
+		[Display(AutoGenerateField = false)]
 		public Department Department { get; }
 
+		[Display(AutoGenerateField = false)]
 		public List<EmployeeView> Employees { get; }
 
+		[Key]
 		public Guid Id => Department.Id;
 
 		public Guid? ParentId => Department.ParentId;
 
+		[Display(Name = "Name", Description = "The name of the department.")]
 		public string Name
 		{
 			get => Department.Name;
 			set
 			{
 				Department.Name = value;
+				Validate();
 				OnPropertyChanged(nameof(Name));
 			}
 		}
 
+		[Display(Name = "Code", Description = "The code of the department.")]
 		public string Code
 		{
 			get => Department.Code;
 			set
 			{
 				Department.Code = value;
+				Validate();
 				OnPropertyChanged(nameof(Code));
 			}
 		}
@@ -47,6 +55,18 @@ namespace EnterpriseViewer.WinForms.Presenters
 		{
 			var handler = PropertyChanged;
 			handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private void Validate()
+		{
+			if (Name == null || Name.Length > 50)
+			{
+				throw new ArgumentException("Incorrect name!");
+			}
+			if (Code?.Length > 10)
+			{
+				throw new ArgumentException("Incorrect code!");
+			}
 		}
 	}
 }
--- EnterpriseViewer/EnterpriseViewer.WinForms/Presenters/EmployeeView.cs
+++ EnterpriseViewer/EnterpriseViewer.WinForms/Presenters/EmployeeView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using EnterpriseViewer.Model;
 
 namespace EnterpriseViewer.WinForms.Presenters
@@ -11,82 +12,101 @@ namespace EnterpriseViewer.WinForms.Presenters
 			Employee = employee ?? throw new ArgumentNullException(nameof(employee));
 		}
 
+		[Display(AutoGenerateField = false)]
 		public Employee Employee { get; }
 
+		[Key]
+		[Display(AutoGenerateField = false)]
 		public decimal Id => Employee.Id;
 
+		[Display(Name = "FullName", Description = "The Fullname of the employee.")]
 		public string FullName => $"{Surname} {FirstName} {Patronymic}";
 
+		[Display(Name = "FirstName", Description = "The firstname of the employee.")]
 		public string FirstName
 		{
 			get => Employee.FirstName;
 			set
 			{
 				Employee.FirstName = value;
+				Validate();
 				OnPropertyChanged(nameof(FirstName));
 			}
 		}
 
+		[Display(Name = "Surname", Description = "The surname of the employee.")]
 		public string Surname
 		{
 			get => Employee.Surname;
 			set
 			{
 				Employee.Surname = value;
+				Validate();
 				OnPropertyChanged(nameof(Surname));
 			}
 		}
 
+		[Display(Name = "Patronymic", Description = "The patronymic of the employee.")]
 		public string Patronymic
 		{
 			get => Employee.Patronymic;
 			set
 			{
 				Employee.Patronymic = value;
+				Validate();
 				OnPropertyChanged(nameof(Patronymic));
 			}
 		}
 
+		[Display(Name = "DocSeries", Description = "The doc series of the employee.")]
 		public string DocSeries
 		{
 			get => Employee.DocSeries;
 			set
 			{
 				Employee.DocSeries = value;
+				Validate();
 				OnPropertyChanged(nameof(DocSeries));
 			}
 		}
 
+		[Display(Name = "DocNumber", Description = "The doc number of the employee.")]
 		public string DocNumber
 		{
 			get => Employee.DocNumber;
 			set
 			{
 				Employee.DocNumber = value;
+				Validate();
 				OnPropertyChanged(nameof(DocNumber));
 			}
 		}
 
+		[Display(Name = "Position", Description = "The position of the employee.")]
 		public string Position
 		{
 			get => Employee.Position;
 			set
 			{
 				Employee.Position = value;
+				Validate();
 				OnPropertyChanged(nameof(Position));
 			}
 		}
 
+		[Display(Name = "DateOfBirth", Description = "Date of birth of the employee.")]
 		public DateTime DateOfBirth
 		{
 			get => Employee.DateOfBirth;
 			set
 			{
 				Employee.DateOfBirth = value;
+				Validate();
 				OnPropertyChanged(nameof(DateOfBirth));
 			}
 		}
 
+		[Display(Name = "Age", Description = "Age of the employee.")]
 		public string Age => $"{GetAge(Employee.DateOfBirth)} years old";
 
 		public event PropertyChangedEventHandler PropertyChanged;
@@ -95,6 +115,38 @@ namespace EnterpriseViewer.WinForms.Presenters
 		{
 			var handler = PropertyChanged;
 			handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private void Validate()
+		{
+			if (Surname == null || Surname.Length > 50)
+			{
+				throw new ArgumentException("Incorrect surname!");
+			}
+			if (FirstName == null || FirstName.Length > 50)
+			{
+				throw new ArgumentException("Incorrect firstname!");
+			}
+			if (Patronymic?.Length > 50)
+			{
+				throw new ArgumentException("Incorrect patronymic!");
+			}
+			if (DocSeries?.Length > 4)
+			{
+				throw new ArgumentException("Incorrect doc series!");
+			}
+			if (DocNumber?.Length > 6)
+			{
+				throw new ArgumentException("Incorrect doc number!");
+			}
+			if (DateOfBirth == null)
+			{
+				throw new ArgumentException("Incorrect date of birth!");
+			}
+			if (Position == null)
+			{
+				throw new ArgumentException("Incorrect position!");
+			}
 		}
 
 		private static int GetAge(DateTime birthDay)
